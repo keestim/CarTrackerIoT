24ddcf4402441cf651149e58a803acf42f4fd380
Tim Keesman <102120418@student.swin.edu.au>
2021-04-19 21:27:48 +1000

    Update SQL

--- CreateTables.sql
+++ CreateTables.sql
@@ -11,12 +11,14 @@ CREATE TABLE Journeys (
 );
 
 CREATE TABLE JourneyDetails (
+  journeyInstanceID INT UNSIGNED NOT NULL AUTO_INCREMENT,
   journeyID INT UNSIGNED NOT NULL,
   latitude FLOAT(4),
   longitude FLOAT(4),
   speed INT,
   RPM INT,
   time DATETIME NOT NULL,
+  PRIMARY KEY (`journeyInstanceID`)
   FOREIGN KEY (`journeyID`) REFERENCES Journeys(`journeyID`)
 );
 
