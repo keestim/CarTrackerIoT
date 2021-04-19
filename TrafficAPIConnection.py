b18c76c7be5e04c000c8a13e14e65c6a068b078a
Tim Keesman <tim.keesman@gmail.com>
2021-04-19 14:47:24 +1000

    Additional Error Handling

--- TrafficAPIConnection.py
+++ TrafficAPIConnection.py
@@ -9,15 +9,17 @@ print(subscriptionKey)
 def GetSpeedLimit(CoordinatesString):
     if CoordinatesString != "-0.0,-0.0":
         api_url = "https://atlas.microsoft.com/search/address/reverse/json?subscription-key=" + subscriptionKey + "&api-version=1.0&query=" + CoordinatesString + "&returnSpeedLimit=true"
+        
+        try:
+            response = requests.get(api_url)
+        finally:
+            map_json_data = response.json()
 
-        response = requests.get(api_url)
-        map_json_data = response.json()
-
-        if response.status_code  == 200:
-            speedLimit = float(map_json_data["addresses"][0]["address"]["speedLimit"].replace("KPH", ""))
-            return speedLimit
-        else:
-            print("api request failed!")
+            if response.status_code == 200:
+                speedLimit = float(map_json_data["addresses"][0]["address"]["speedLimit"].replace("KPH", ""))
+                return speedLimit
+            else:
+                print("api request failed!")
 
 #example usage:
 #print(GetSpeedLimit("-37.799454954912115,145.14715608507657"))
